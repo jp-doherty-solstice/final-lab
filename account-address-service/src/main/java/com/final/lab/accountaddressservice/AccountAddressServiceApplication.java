6af326f688107b694f66d57ed784dcf773ace7f0
John Doherty <jpdoherty@solstice.com>
2018-09-10 14:35:42 -0500

    inital commit

---
+++ account-address-service/src/main/java/com/final/lab/accountaddressservice/AccountAddressServiceApplication.java
@@ -0,0 +1,12 @@
+package com.final.lab.accountaddressservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountAddressServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountAddressServiceApplication.class, args);
+	}
+}
