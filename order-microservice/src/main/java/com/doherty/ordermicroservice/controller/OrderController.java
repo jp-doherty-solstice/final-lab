3227f8b01667d7b9c0b6765f57f5373ec09699fa
John Doherty <jpdoherty@solstice.com>
2018-09-12 08:52:04 -0500

    added shipment and product microservices

--- order-microservice/src/main/java/com/doherty/ordermicroservice/controller/OrderController.java
+++ order-microservice/src/main/java/com/doherty/ordermicroservice/controller/OrderController.java
@@ -1,10 +1,33 @@
 package com.doherty.ordermicroservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.doherty.ordermicroservice.entity.LineItem;
+import com.doherty.ordermicroservice.entity.OrderDetail;
+import com.doherty.ordermicroservice.repository.LineItemRepository;
+import com.doherty.ordermicroservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
 
-    
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    LineItemRepository lineItemRepository;
+
+    @PostMapping("/orders/{id}/line")
+    public void addLineItemForOrder(@PathVariable Long id, @RequestBody LineItem lineItem) {
+        OrderDetail order = orderRepository.getOne(id);
+        lineItem.setOrderDetail(order);
+        lineItemRepository.save(lineItem);
+    }
+
+    @GetMapping("/orders/{id}/lines")
+    public List<LineItem> getLineItemsForOrder(@PathVariable Long id) {
+        return lineItemRepository.findAllLineItemsForOrder(id);
+    }
 
 }
--- order-microservice/src/main/java/com/doherty/ordermicroservice/repository/LineItemRepository.java
+++ order-microservice/src/main/java/com/doherty/ordermicroservice/repository/LineItemRepository.java
@@ -2,8 +2,15 @@ package com.doherty.ordermicroservice.repository;
 
 import com.doherty.ordermicroservice.entity.LineItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LineItemRepository extends JpaRepository<LineItem, Long> {
+
+    @Query("select l from LineItem l where l.orderDetail.orderNumber = ?1")
+    List<LineItem> findAllLineItemsForOrder(Long id);
+
 }
