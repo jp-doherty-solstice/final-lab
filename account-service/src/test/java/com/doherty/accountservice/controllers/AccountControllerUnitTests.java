f0f7d89b21ae93effd157bb07e1beeac4b0d7a3a
John Doherty <jpdoherty@solstice.com>
2018-09-24 08:39:16 -0500

    started testing account service and controller

--- account-service/src/test/java/com/doherty/accountservice/controllers/AccountControllerUnitTests.java
+++ account-service/src/test/java/com/doherty/accountservice/controllers/AccountControllerUnitTests.java
@@ -1,13 +1,25 @@
 package com.doherty.accountservice.controllers;
 
+import com.doherty.accountservice.entities.Address;
+import com.doherty.accountservice.services.AccountService;
+import com.doherty.accountservice.services.AddressService;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(AccountController.class)
@@ -16,30 +28,44 @@ public class AccountControllerUnitTests {
     @Autowired
     private MockMvc mockMvc;
 
-//    @Mock
-//    AccountRepository accountRepository;
-//
-//    @Mock
-//    AddressRepository addressRepository;
+    @MockBean
+    AccountService accountService;
+
+    @MockBean
+    AddressService addressService;
 
     @InjectMocks
     AccountController accountController;
 
-//    public Address address;
-//
-//    @Before
-//    public void setUp() {
-//        MockitoAnnotations.initMocks(this);
-//        address = new Address();
-//        address.setStreet("Mystic");
-//        address.setApt("1B");
-//        address.setCity("Bedford");
-//        address.setState("IL");
-//        address.setZipcode("76859");
-//        address.setCountry("USA");
-//    }
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
 
+    @Test
+    public void addAddressForAccount_ReturnsValidAddress() throws Exception {
 
+        Address address = createDummyAddress();
+
+        when(accountController.addAddressForAccount(any(Long.class), any(Address.class)))
+                .thenReturn(address);
+
+        mockMvc.perform(post("/accounts/5/address", address))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("state", "MA"))
+                .andReturn();
+    }
+
+    private Address createDummyAddress() {
+        Address address = new Address();
+        address.setStreet("20 Grace Road");
+        address.setApt("C4");
+        address.setCity("Oak Brook");
+        address.setState("MA");
+        address.setZipcode("02543");
+        address.setCountry("USA");
+        return address;
+    }
 
 
 
--- account-service/src/test/java/com/doherty/accountservice/services/AddressServiceUnitTests.java
+++ account-service/src/test/java/com/doherty/accountservice/services/AddressServiceUnitTests.java
@@ -1,16 +1,58 @@
 package com.doherty.accountservice.services;
 
+import com.doherty.accountservice.entities.Address;
 import com.doherty.accountservice.repository.AddressRepository;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class AddressServiceUnitTests {
 
     @Mock
     private AddressRepository addressRepository;
+
+    @InjectMocks
+    private AddressService addressService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void findAddressesForAccount_WithValidInput_ReturnsTheAddresses() {
+        List<Address> addresses = createDummyAddressList();
+        when(addressRepository.findAddressesForAccount(any(Long.class))).thenReturn(addresses);
+        List<Address> foundAddressList = addressService.findAllAddressesForAccount(5L);
+        assertEquals(foundAddressList.get(1).getCity(), "Nashville");
+    }
+
+    private List<Address> createDummyAddressList() {
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(createDummyAddress("Chicago", "IL", "67547"));
+        addresses.add(createDummyAddress("Nashville", "TN", "32547"));
+        addresses.add(createDummyAddress("Las Vegas", "NV", "98547"));
+        return addresses;
+    }
+
+    private Address createDummyAddress(String city, String state, String zip) {
+        Address address = new Address();
+        address.setCity(city);
+        address.setState(state);
+        address.setZipcode(zip);
+        return address;
+    }
 
 }
